65e06273957f0e1123c397920b739f0f67c4f160
aalena <aalinashr@gmail.com>
2019-07-07 12:11:43 +0545

    - changed environment file

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: '',
+    modulePrefix: 'empress-blog-casper',
     environment,
     rootURL: '/',
     locationType: 'auto',
